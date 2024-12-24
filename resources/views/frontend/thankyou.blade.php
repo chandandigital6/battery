1af7b65ea1b5550d184253ddf39e422b5d019c31
kajal kumari <2005050100024kajal@gmail.com>
2024-12-24 16:49:32 +0530

    add

--- resources/views/frontend/thankyou.blade.php
+++ resources/views/frontend/thankyou.blade.php
@@ -1,5 +1,5 @@
 @extends('Components.main')
-@section('comtent')
+@section('content')
 <div class="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-500 to-black-500">
     <div class="text-center text-white p-10 rounded-lg shadow-lg bg-opacity-15 bg-gray-800">
         <h1 class="text-4xl font-bold mb-4">Thank You!</h1>
