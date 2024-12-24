59bdb3351ffa0a11b57df8fd49594ddff8d62f07
kajal kumari <2005050100024kajal@gmail.com>
2024-12-24 16:43:19 +0530

    s

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -78,11 +78,11 @@
             @foreach ($abouts->skip(1)->take(3) as $ab)
 
 
-            <div class="p-6 bg-gradient-to-r from-green-500 to-black rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+            <div class="p-6 bg-gradient-to-r from-green-500 to-black text-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
                 {{-- <div class="flex justify-center"><img src="{{asset('storage/'.$ab->image)}}" alt="" class="h-24 w-24"></div> --}}
                 <h3 class="text-2xl font-semibold text-white">{{ $ab->title }}</h3>
                 <p class="mt-4 text-white">
-       {!! $ab->description !!}
+                  {!! $ab->description !!}
                 </p>
             </div>
             @endforeach
@@ -234,12 +234,12 @@
           <!-- Left Section -->
           <div class="max-w-lg text-center sm:text-left">
             <h2 class=" text-xl mt-4 md:text-4xl font-bold text-gray-100 tracking-tight">
-              A smile <br class="hidden sm:block lg:hidden"> is an inexpensive way to change your looks
+              What our <br class="hidden sm:block lg:hidden"> clients say about the Litho Power batteries
             </h2>
-            <p class="mt-4 text-gray-300">
+            {{-- <p class="mt-4 text-gray-300">
               Goals are just dreams with a deadline
-            </p>
-            <div class="flex flex-row items-center space-x-3 mt-5 ml-12">
+            </p> --}}
+            {{-- <div class="flex flex-row items-center space-x-3 mt-5 ml-12">
               <!-- Social Buttons -->
               <a href="#" target="_blank" class="w-10 h-10 flex items-center justify-center rounded-2xl font-bold text-lg bg-green-600 hover:drop-shadow-lg cursor-pointer transition ease-in duration-300">
                 <svg class="w-4 fill-gray-100" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -251,7 +251,7 @@
                   <path d="M22 15.047a.846.846 0 0 1-.008.112l-.006.037-.016.072c-.003.015-.008.028-.013.042l-.022.063-.02.042c-.008.02-.018.038-.028.057l-.025.04a.769.769 0 0 1-.108.135l-.035.034a.812.812 0 0 1-.049.04l-.038.03c-.005.003-.01.008-.015.01l-9.14 6.095a.858.858 0 0 1-.954 0l-9.14-6.094-.014-.01a.807.807 0 0 1-.088-.071c-.012-.01-.023-.022-.034-.034-.015-.015-.03-.03-.043-.046a.707.707 0 0 1-.066-.09 1.038 1.038 0 0 1-.054-.096l-.019-.042a.868.868 0 0 1-.022-.063c-.005-.014-.01-.027-.013-.042-.007-.023-.01-.048-.015-.072l-.007-.037A.847.847 0 0 1 2 15.047V8.953c0-.038.003-.075.008-.112l.007-.037c.004-.024.008-.049.015-.072a.774.774 0 0 1 .145-.295.978.978 0 0 1 .029-.038l.043-.046.034-.034a.834.834 0 0 1 .088-.07c.005-.003.009-.008.014-.01l9.14-6.095a.86.86 0 0 1 .954 0l9.14 6.094c.005.003.01.008.015.01l.038.03a.839.839 0 0 1 .05.041l.034.034a.735.735 0 0 1 .108.136l.025.04.029.056.019.042.022.063c.005.014.01.027.013.042.007.023.011.048.016.072l.006.037a.834.834 0 0 1 .008.112v6.094ZM3.719 10.562v2.876L5.869 12l-2.15-1.438Zm7.422-2.088V4.465l-6.734 4.49 3.008 2.011 3.726-2.492Zm8.452.48L12.86 4.465v4.009l3.726 2.492 3.007-2.012ZM4.407 15.046l6.734 4.489v-4.009l-3.726-2.492-3.008 2.012Zm8.453.48v4.009l6.733-4.49-3.007-2.01-3.726 2.491ZM12 9.966 8.96 12 12 14.033 15.04 12 12 9.967Zm8.281 3.472v-2.876L18.131 12l2.15 1.438Z"/>
                 </svg>
               </a>
-            </div>
+            </div> --}}
           </div>
 
           <!-- Right Section -->
